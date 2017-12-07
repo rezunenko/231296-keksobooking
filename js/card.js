84e18ad2f33d8a9ce063eb7283f32a5771524e37
rezunenko <tamerlan88111@yandex.ru>
2017-12-08 00:49:08 +0300

    split js code to modules

---
+++ js/card.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function() {
+  var ESC_KEYCODE = 27;
+  var mapPopupTemplate = document.querySelector('template').content.querySelector('article.map__card');
+  var map = document.querySelector('.map');
+  window.activePin = null;
+
+  window.renderMapPopup = function(post) {
+    var popupTemplate = mapPopupTemplate.cloneNode(true);
+    var dictionary = {
+      'flat': 'Квартира',
+      'bungalo': 'Бунгало',
+      'house': 'Дом'
+    };
+    var postType = popupTemplate.querySelector('h4');
+    var features = popupTemplate.querySelector('.popup__features');
+    var featuresFragment = document.createDocumentFragment();
+    popupTemplate.querySelector('.popup__avatar').src = post.author.avatar;
+    popupTemplate.querySelector('h3').textContent = post.offer.title;
+    popupTemplate.querySelector('small').textContent = post.offer.address;
+    popupTemplate.querySelector('.popup__price').innerHTML = post.offer.price + '&#x20bd;/ночь';
+    postType.textContent = dictionary[post.offer.type];
+    postType.nextElementSibling.textContent = post.offer.rooms + ' комнаты для ' + post.offer.guests + ' гостей';
+    postType.nextElementSibling.nextElementSibling.textContent = 'заезд после ' + post.offer.checkin + ' , выезд до ' + post.offer.checkout;
+    popupTemplate.querySelector('.popup__features').innerHTML = '';
+    for (var i = 0; i < post.offer.features.length; i++) {
+      var li = document.createElement('li');
+      li.className = 'feature  feature--' + post.offer.features[i];
+      featuresFragment.appendChild(li);
+    }
+    features.appendChild(featuresFragment);
+    features.nextElementSibling.textContent = post.offer.description;
+
+    return popupTemplate;
+  };
+
+  var onPopupClose = function(e) {
+    var popup = document.querySelector('.map__card.popup');
+    if (e.type === 'keydown' && e.keyCode !== ESC_KEYCODE
+      || e.type === 'click' && !e.target.classList.contains('popup__close')) {
+      return;
+    }
+
+    popup.setAttribute('hidden', '');
+    popup.removeEventListener('click', onPopupClose);
+    document.removeEventListener('keydown', onPopupClose);
+    window.activePin.classList.remove('map__pin--active');
+  };
+
+  window.showPopup = function(object) {
+    var oldPopup = map.querySelector('.map__card');
+
+    if (oldPopup) {
+      map.removeChild(oldPopup);
+    }
+    var newPopupElement = map.appendChild(object);
+    newPopupElement.addEventListener('click', onPopupClose);
+    document.addEventListener('keydown', onPopupClose);
+  };
+})();
---
+++ js/pin.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  var PIN_SIZE = 40;
+  var PIN_TAIL_HEIGHT = 18;
+
+  window.createPin = function (post) {
+    var btn = document.createElement('button');
+    var img = document.createElement('img');
+    btn.style.left = post.location.x + 'px';
+    btn.style.top = (post.location.y - (PIN_SIZE + PIN_TAIL_HEIGHT) / 2) + 'px';
+    btn.className = 'map__pin';
+    btn.setAttribute('data-id', post.id);
+    img.src = post.author.avatar;
+    img.width = PIN_SIZE;
+    img.height = PIN_SIZE;
+    img.draggable = false;
+    btn.appendChild(img);
+
+    return btn;
+  };
+
+})();
